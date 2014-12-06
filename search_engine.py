7ffe22aa3e326cbf8c63e971d5f002953f0fb1af
Alexander Putilin <avp-13@yandex.ru>
2014-12-07 00:14:37 +0300

    Crawler now saves stuff, and can go to next pages

--- search_engine.py
+++ search_engine.py
@@ -1,13 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
+import logging
+import time
+import re
+import os.path
+from base64 import b16encode
 
-def downloadUrl(url):
-     r = requests.get(url)
-     if r.status_code != 200:
-         raise Exception("Non-OK status code: {}".format(r.status_code))
-     return r.text
+# TODO is it good idea to set it at this point? 
+logging.getLogger().setLevel(logging.DEBUG)
 
-def parseText(html):
+_r_learnprogramming_url = re.compile(r'http://(www.)?reddit.com/r/learnprogramming')
+
+def downloadRedditUrl(url):
+    logging.debug("Downloading url: {}".format(url))
+    assert _r_learnprogramming_url.match(url)
+    headers = { 'User-Agent': 'SearchingReddit bot version 0.1'}
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        raise Exception("Non-OK status code: {}".format(r.status_code))
+    return r.text
+
+def parseRedditPost(html):
     bs = BeautifulSoup(html)
     return bs.select('div.usertext-body')[1].text
 
+class Crawler(object):
+    def __init__(self, start_url, storage_dir):
+        self.start_url = start_url 
+        self.storage_dir = storage_dir
+
+    @staticmethod
+    def _make_absolute_url(url):
+        return 'http://reddit.com' + url
+
+    def crawl(self):
+        logging.debug("Starting to crawl from page {}".format(self.start_url))
+        current_page_url = self.start_url
+        while True:
+            logging.debug("current page is {}".format(current_page_url))
+            current_page = downloadRedditUrl(current_page_url)
+            bs = BeautifulSoup(current_page)
+            all_posts_links = bs.findAll('a',attrs={'class':'title'});
+            post_links = [Crawler._make_absolute_url(link['href']) for link in all_posts_links]
+            for post_link in post_links:
+                html = downloadRedditUrl(post_link)
+                stored_text_file_name = os.path.join(self.storage_dir,
+                        b16encode(post_link))
+                stored_text_file = open(stored_text_file_name, "w")
+                stored_text_file.write(html.encode('utf8'))
+                time.sleep(2)
+
+            next_page_url = bs.find('a', attrs={'rel' : 'next'})['href']
+            logging.debug("First post is {}".format(post_links[0]))
+            current_page_url = next_page_url
+            time.sleep(2)
