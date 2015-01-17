7ea1060b16d689d4f976d6b2f1317267dd7debe8
Alexander Putilin <avp-13@yandex.ru>
2015-01-17 15:51:36 -0500

    Speedup parseRedditPost by about 10%

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import requests
 import re
 import logging
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 _r_learnprogramming_url = re.compile(r'http://(www.)?reddit.com/r/learnprogramming')
 
 def downloadRedditUrl(url):
@@ -13,6 +13,8 @@ def downloadRedditUrl(url):
         raise Exception("Non-OK status code: {}".format(r.status_code))
     return r.text
 
+_only_div_usertext_body = SoupStrainer("div", {'class': lambda L: L is not None and 'usertext-body' in L.split()})
+
 def parseRedditPost(html):
-    bs = BeautifulSoup(html)
+    bs = BeautifulSoup(html,parse_only=_only_div_usertext_body)
     return bs.select('div.usertext-body')[1].text
