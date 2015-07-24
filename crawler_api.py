6d25b6d4c6c363de8cf5802d0104ae29875374c5
Alexander Putilin <avp-13@yandex.ru>
2015-07-24 19:27:16 +0300

    Replace more comments in crawler_api.py (though the file isn't really needed)

--- crawler_api.py
+++ crawler_api.py
@@ -21,6 +21,7 @@ def get_as_much_stuff_as_possible(storage_dir):
 def crawl_continuously(storage_dir):
     r = praw.Reddit(user_agent='SearchingReddit project 0.2 by /u/godlikesme')
     for s in submission_stream(r, "learnprogramming"):
+        s.replace_more_comments(limit=None)
         save_submission(s, storage_dir)
 
 
